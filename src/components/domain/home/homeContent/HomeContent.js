0e6c1f460bf52d8a33f2bc9caaea588769cf6b42
Gia-Kiet-99 <kietdg99@gmail.com>
2021-07-12 09:53:01 +0700

    add intro image in home page

--- src/components/domain/home/homeContent/HomeContent.js
+++ src/components/domain/home/homeContent/HomeContent.js
@@ -63,23 +63,29 @@ export default function HomeContent() {
     <div className='container-fluid flex-fill'>
       <div className='row'>
         <div className='col-8 m-auto'>
+          <div className='rounded overflow-hidden'>
+            {/*https://blog.blackboard.com/wp-content/uploads/Blog_Post-1.png*/}
+            <img src="https://devinfo.in/wp-content/uploads/2021/05/LIL.png"
+                 height="450px" width='100%' alt="intro.png"/>
+          </div>
+
           {/* Top featured courses */}
-          <Section title='Featured Courses' className='mt-4'>
+          <Section title='Featured Courses' className='mt-5'>
             <CarouselList courseList={featuredCourses} className='mt-3'/>
           </Section>
 
           {/* Latest courses */}
-          <Section title='Latest Courses' className='mt-4'>
+          <Section title='Latest Courses' className='mt-5'>
             <CarouselList courseList={latestCourses} className='mt-3'/>
           </Section>
 
           {/* Most viewed courses */}
-          <Section title='Most Viewed Course' className='mt-4'>
+          <Section title='Most Viewed Course' className='mt-5'>
             <CarouselList courseList={mostViewedCourses} className='mt-3'/>
           </Section>
 
           {/*/!* Top featured category *!/*/}
-          <Section title='Featured Category' className='mt-4'>
+          <Section title='Featured Category' className='mt-5'>
             <ButtonList
               className='mt-3'
               titleList={featuredCategories.map(e => e.categoryName)}/>
