345ad991f8b1d3cf3d39f1fb3b288d0aa0393f9c
Gia-Kiet-99 <kietdg99@gmail.com>
2021-07-29 22:19:10 +0700

    Upload video: show propress

--- src/components/domain/lecturer/newLessonForm/NewLessonForm.js
+++ src/components/domain/lecturer/newLessonForm/NewLessonForm.js
@@ -1,12 +1,15 @@
 import React, {useContext, useState} from 'react';
 import {useForm} from "react-hook-form";
 import {uploadVideoContext} from "provider/uploadVideoProvider";
+import {Spinner} from "react-bootstrap";
 
 const NewLessonForm = ({className, onCancel, parentSectionId, courseId, onUploaded}) => {
   const {state: uploadState, event} = useContext(uploadVideoContext);
   const {register, handleSubmit} = useForm();
   const [lessonUrl, setLessonUrl] = useState("");
   const [duration, setDuration] = useState(0);
+  const [onUpload, setOnUpload] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const onSubmit = (data) => {
     if (!data.title || !lessonUrl || !duration) {
@@ -23,23 +26,27 @@ const NewLessonForm = ({className, onCancel, parentSectionId, courseId, onUpload
       totalLength: duration,
       videoUrl: lessonUrl,
       order: order,
-      // isPreview: true
+      isPreview: data.isPreview
     }, onUploaded);
     hideForm();
   }
 
   const onProgress = (bytes_uploaded, bytes_total) => {
-    let percentage = (bytes_uploaded / bytes_total * 100).toFixed(2);
-    console.log(bytes_uploaded, bytes_total, percentage + '%');
+    // let percentage = (bytes_uploaded / bytes_total * 100).toFixed(2);
+    // console.log(bytes_uploaded, bytes_total, percentage + '%');
+    let percentage = Math.round((bytes_uploaded / bytes_total * 100));
+    setProgress(percentage);
   }
 
   const onVideoUploaded = (uri) => {
-    console.log('Your video URI is: ' + uri);
     const id = uri.split("/").slice(-1)[0];
+    setOnUpload(false);
+    setProgress(0);
     setLessonUrl(`https://vimeo.com/${id}`);
   }
 
   const handleVideoChange = (e) => {
+    setOnUpload(true);
     const file = e.target.files[0];
     getVideoDuration(file);
     event.uploadVideo(file, onProgress, onVideoUploaded);
@@ -64,14 +71,24 @@ const NewLessonForm = ({className, onCancel, parentSectionId, courseId, onUpload
   return (
     <div className={classes}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="d-flex align-items-center">
+        <div className="d-flex">
           <label htmlFor="lesson-title"
-                 className="text-smaller font-weight-bold m-0 mr-2">
+                 className="text-smaller font-weight-bold m-0 mr-2 pt-1">
             New Lesson
           </label>
-          <input type="text" {...register("title", {required: true})}
-                 className="flex-grow-1 input-text"
-                 placeholder="Enter a Title"/>
+          <div className="flex-grow-1 text-smaller">
+            <input
+              type="text" {...register("title", {required: true})}
+              className="d-block w-100 input-text"
+              placeholder="Enter a Title"/>
+            <div className="d-flex align-items-center my-2 mr-1">
+              <input
+                id="isPreviewCheckbox"
+                type="checkbox"
+                {...register("isPreview", {required: false})}/>
+              <label htmlFor="isPreviewCheckbox" className="m-0 ml-1">Public lesson</label>
+            </div>
+          </div>
         </div>
         <div className="text-right mt-3">
           <button
@@ -81,7 +98,21 @@ const NewLessonForm = ({className, onCancel, parentSectionId, courseId, onUpload
           </button>
           <button type="button" style={{fontSize: 15}}
                   className="pure-button btn-dark font-weight-bold mr-2">
-            <label htmlFor="upload-lesson" className="m-0">+ Video</label>
+            {onUpload ? (
+              <>
+                <Spinner
+                  as="span"
+                  animation="border"
+                  size="sm"
+                  role="status"
+                  aria-hidden="true"
+                />
+                <span className="ml-1">Uploading... {progress}%</span>
+              </>
+            ) : (
+              <label htmlFor="upload-lesson" className="m-0">+ Video</label>
+            )}
+
           </button>
           <input id="upload-lesson" type="file" accept='video/*'
                  onChange={handleVideoChange} hidden/>
